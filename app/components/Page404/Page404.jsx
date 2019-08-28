7737a992c5c12f292c5db5f4a94ce094386f4df0
Rostislav Gogolauri 'mindfulme <alice.gogolauri@gmail.com>
2019-08-28 18:46:41 +0300

    deployed by vs git bot

--- app/components/Page404/Page404.jsx
+++ app/components/Page404/Page404.jsx
@@ -40,7 +40,7 @@ class Page404 extends React.Component {
                         <Translate content={"page404." + this.props.subtitle} />
                     </div>
                     <div className="page-404-button-back">
-                        <Link to={"/"}>
+                        <Link style={{color: "white"}} to={"/"}>
                             <Translate
                                 component="button"
                                 className="button"
