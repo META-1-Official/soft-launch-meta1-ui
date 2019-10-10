4cc47a2d3007f6f773bfb0de0b855761eb531211
Rostislav Gogolauri 'mindfulme <alice.gogolauri@gmail.com>
2019-10-10 11:23:03 +0300

    updated btc withdraw endpoint

--- app/components/Modal/WithdrawModalMeta.jsx
+++ app/components/Modal/WithdrawModalMeta.jsx
@@ -41,7 +41,7 @@ class WithdrawModalContent extends DecimalChecker {
                     keys._root.entries[0][0]
                 );
                 let privatekey = private_key.toWif();
-                fetch("https://asterope.meta-exchange.info/wbtc", {
+                fetch("https://asterope.meta-exchange.info/testnet-wbtc", {
                     method: "POST",
                     headers: {
                         Accept: "application/json, text/plain, */*",
