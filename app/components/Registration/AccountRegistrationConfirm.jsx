426188aaaed97e42f4296d954d449e13e6431985
mindfulme <ceo@citvy.com>
2020-07-30 23:54:19 +0300

    rm terms moddal

--- app/components/Registration/AccountRegistrationConfirm.jsx
+++ app/components/Registration/AccountRegistrationConfirm.jsx
@@ -52,33 +52,33 @@ class AccountRegistrationConfirm extends React.Component {
         );
     }
 
-    componentDidMount() {
-        // Get the modal
-        var modal = document.getElementById("myModal");
+    // componentDidMount() {
+    //     // Get the modal
+    //     var modal = document.getElementById("myModal");
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
+    //     // Get the button that opens the modal
+    //     // var btn = document.getElementById("myBtn");
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+    //     // // Get the <span> element that closes the modal
+    //     // var span = document.getElementsByClassName("close")[0];
 
-        // When the user clicks the button, open the modal
-        btn.onclick = function() {
-            modal.style.display = "block";
-        };
+    //     // // When the user clicks the button, open the modal
+    //     // btn.onclick = function() {
+    //     //     modal.style.display = "block";
+    //     // };
 
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        };
+    //     // When the user clicks on <span> (x), close the modal
+    //     span.onclick = function() {
+    //         modal.style.display = "none";
+    //     };
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        };
-    }
+    //     // When the user clicks anywhere outside of the modal, close it
+    //     window.onclick = function(event) {
+    //         if (event.target == modal) {
+    //             modal.style.display = "none";
+    //         }
+    //     };
+    // }
 
     componentWillMount() {
         console.log("session #:", sessionStorage.getItem("email"));
@@ -245,8 +245,8 @@ class AccountRegistrationConfirm extends React.Component {
                         onChange={this.toggleConfirmedTerms}
                     >
                         &nbsp;&nbsp;&nbsp;
-                        <button className="reset-this terms" id="myBtn">
-                            I accept terms of use.
+                        <button className="reset-this terms">
+                            I am a living man or woman hence a living being.
                         </button>
                     </Checkbox>
                     <div id="myModal" class="custom-modal">
