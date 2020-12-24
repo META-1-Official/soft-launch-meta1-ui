db70bcda88409410747e216899547e904e92feb2
mindfulme <go.go.gg.rostislav@gmail.com>
2020-12-24 11:23:40 +0200

    fix typo

--- app/components/Modal/DepositModalXlm.jsx
+++ app/components/Modal/DepositModalXlm.jsx
@@ -154,7 +154,7 @@ export default class DepositModal extends React.Component {
     }
 
     getDepositAddress() {
-        fetch("https://api.meta1.io/api")
+        fetch("https://gateway.meta1.io/api")
             .then(response => {
                 fetch("https://gateway.meta1.io/api/wallet/init/xlm", {
                     method: "POST",
