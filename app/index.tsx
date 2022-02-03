1c2e0706ba0453c363b88eafd936a77aba14dab0
Zoran <zokiupwork@gmail.com>
2022-02-03 16:17:07 +0500

    refactoring-branch-b | set layout | set footer and header for all pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AppInit from "./AppInit";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppInit from './AppInit';
+import {ThemeProvider} from '@emotion/react';
+import darkTheme from './lib/styles/themeDark';
+
 if (__PERFORMANCE_DEVTOOL__) {
-  const {registerObserver} = require("react-perf-devtool");
-  registerObserver();
+	const {registerObserver} = require('react-perf-devtool');
+	registerObserver();
 }
 
-const rootEl = document.getElementById("content");
+const rootEl = document.getElementById('content');
 const render = () => {
-  ReactDOM.render(<AppInit />, rootEl);
+	ReactDOM.render(
+		<ThemeProvider theme={darkTheme}>
+			<AppInit />
+		</ThemeProvider>,
+		rootEl
+	);
 };
 render();
