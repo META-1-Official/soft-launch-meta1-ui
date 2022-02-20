5f802b542d2f91644f896f6788593ce59f208ded
Zoran <zokiupwork@gmail.com>
2022-02-20 13:09:57 +0500

    refactoring-branch-b | set hover effect in header --fix

--- app/lib/styles/GlobalStyles.tsx
+++ app/lib/styles/GlobalStyles.tsx
@@ -18,6 +18,7 @@ const GlobalStyles = (theme: any) => css`
 
 		.ant-menu {
 			color: ${theme.colors.white};
+			background: ${theme.colors.sideBar} !important;
 			.ant-menu-item-selected,
 			.ant-menu-item-selected > a,
 			.ant-menu-item-selected > a:hover {
@@ -29,34 +30,26 @@ const GlobalStyles = (theme: any) => css`
 			.ant-menu-item:hover {
 				color: ${theme.colors.primaryColor} !important;
 			}
-			.ant-menu-inline .ant-menu-item:after {
-				border-right: 3px solid ${theme.colors.primaryColor} !important;
+		}
+		.ant-menu-inline .ant-menu-item:after {
+			border-right: 3px solid ${theme.colors.primaryColor} !important;
+		}
+		.ant-menu-horizontal {
+			border-bottom: 0px;
+			.ant-menu-item-selected {
+				background-color: transparent !important;
 			}
-			.ant-menu-horizontal {
-				background: ${theme.colors.sideBar} !important;
-				border-bottom: 0px;
-				.ant-menu-item-selected {
-					background-color: transparent !important;
-				}
-				.ant-menu-item:hover {
-					color: ${theme.colors.primaryColor} !important;
-				}
-				.ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item:hover::after,
-				.ant-menu-horizontal:not(.ant-menu-dark)
-					> .ant-menu-submenu:hover::after,
-				.ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item-active::after,
-				.ant-menu-horizontal:not(.ant-menu-dark)
-					> .ant-menu-submenu-active::after,
-				.ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item-open::after,
-				.ant-menu-horizontal:not(.ant-menu-dark)
-					> .ant-menu-submenu-open::after,
-				.ant-menu-horizontal:not(.ant-menu-dark)
-					> .ant-menu-item-selected::after,
-				.ant-menu-horizontal:not(.ant-menu-dark)
-					> .ant-menu-submenu-selected::after {
-					border-bottom: 3px solid ${theme.colors.primaryColor} !important;
-				}
-			}
+		}
+		.ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item:hover::after,
+		.ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-submenu:hover::after,
+		.ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item-active::after,
+		.ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-submenu-active::after,
+		.ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item-open::after,
+		.ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-submenu-open::after,
+		.ant-menu-horizontal:not(.ant-menu-dark) > .ant-menu-item-selected::after,
+		.ant-menu-horizontal:not(.ant-menu-dark)
+			> .ant-menu-submenu-selected::after {
+			border-bottom: 3px solid ${theme.colors.primaryColor} !important;
 		}
 	}
 `;
