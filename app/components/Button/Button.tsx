1777ff501870308ffb3e3f81a54c016c386914c3
Zoran <zokiupwork@gmail.com>
2022-04-04 00:15:04 +0500

    assets-redesign | change table footer and pagination design

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -5,8 +5,7 @@ import React from 'react';
 interface IStyledButton {
 	children: React.ReactNode;
 	shape: 'circle' | 'round' | 'default' | undefined;
-	gray: boolean;
-	buttonType: 'primary' | 'gray' | 'transparent';
+	buttonType: 'primary' | 'white' | 'transparent' | 'red';
 }
 
 const StyledButton = ({
@@ -23,14 +22,18 @@ const StyledButton = ({
 		backgroundColor = theme.colors.primaryColor;
 		border = `1px solid ${theme.colors.primaryColor}`;
 		color = 'black';
-	} else if (buttonType === 'gray') {
+	} else if (buttonType === 'white') {
 		backgroundColor = 'transparent';
-		border = `1px solid ${theme.colors.buttonGrayColor}`;
-		color = theme.colors.buttonGrayColor;
+		border = `1px solid ${theme.colors.buttonWhiteColor}`;
+		color = theme.colors.buttonWhiteColor;
 	} else if (buttonType === 'transparent') {
 		backgroundColor = 'transparent';
 		border = `1px solid ${theme.colors.primaryColor}`;
 		color = theme.colors.primaryColor;
+	} else if (buttonType === 'red') {
+		backgroundColor = theme.colors.buttonRedColor;
+		border = `1px solid ${theme.colors.buttonRedColor}`;
+		color = 'white';
 	}
 
 	return (
