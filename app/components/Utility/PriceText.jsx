27a956069f39ea2e40bd2827c21ff84e54a8ad11
Kok <kok@Koks-Mac-mini.local>
2022-04-21 02:29:58 +0800

    fixed some bugs

--- app/components/Utility/PriceText.jsx
+++ app/components/Utility/PriceText.jsx
@@ -1,67 +1,49 @@
-import React from "react";
-import utils from "common/utils";
+import React from 'react';
+import utils from 'common/utils';
 
 class PriceText extends React.Component {
-    render() {
-        let {price, preFormattedPrice, quote, base} = this.props;
-        if (!price && !preFormattedPrice) return null;
-        let formattedPrice = !!preFormattedPrice
-            ? preFormattedPrice
-            : utils.price_to_text(price, quote, base);
+	render() {
+		let {price, preFormattedPrice, quote, base} = this.props;
+		if (!price && !preFormattedPrice) return <span>0.0</span>;
+		let formattedPrice = !!preFormattedPrice
+			? preFormattedPrice
+			: utils.price_to_text(price, quote, base);
 
-        if (formattedPrice.full >= 1) {
-            return (
-                <span>
-                    &nbsp;&nbsp;
-                    <span className="price-integer">{formattedPrice.int}.</span>
-                    {formattedPrice.dec ? (
-                        <span className="price-integer">
-                            {formattedPrice.dec}
-                        </span>
-                    ) : null}
-                    {formattedPrice.trailing ? (
-                        <span className="price-decimal">
-                            {formattedPrice.trailing}
-                        </span>
-                    ) : null}
-                </span>
-            );
-        } else if (formattedPrice.full >= 0.1) {
-            return (
-                <span>
-                    &nbsp;&nbsp;
-                    <span className="price-decimal">{formattedPrice.int}.</span>
-                    {formattedPrice.dec ? (
-                        <span className="price-integer">
-                            {formattedPrice.dec}
-                        </span>
-                    ) : null}
-                    {formattedPrice.trailing ? (
-                        <span className="price-decimal">
-                            {formattedPrice.trailing}
-                        </span>
-                    ) : null}
-                </span>
-            );
-        } else {
-            return (
-                <span>
-                    &nbsp;&nbsp;
-                    <span className="price-decimal">{formattedPrice.int}.</span>
-                    {formattedPrice.dec ? (
-                        <span className="price-decimal">
-                            {formattedPrice.dec}
-                        </span>
-                    ) : null}
-                    {formattedPrice.trailing ? (
-                        <span className="price-integer">
-                            {formattedPrice.trailing}
-                        </span>
-                    ) : null}
-                </span>
-            );
-        }
-    }
+		if (formattedPrice.full >= 1) {
+			return (
+				<span>
+					&nbsp;&nbsp;
+					<span className="price-integer">{formattedPrice.int}.</span>
+					<span className="price-integer">{formattedPrice.dec ?? '0.0'}</span>
+					<span className="price-decimal">
+						{formattedPrice.trailing ?? '0.0'}
+					</span>
+				</span>
+			);
+		} else if (formattedPrice.full >= 0.1) {
+			return (
+				<span>
+					&nbsp;&nbsp;
+					<span className="price-decimal">{formattedPrice.int}.</span>
+					<span className="price-integer">{formattedPrice.dec ?? '0.0'}</span>
+					<span className="price-decimal">
+						{formattedPrice.trailing ?? '0.0'}
+					</span>
+				</span>
+			);
+		} else {
+			return (
+				<span>
+					&nbsp;&nbsp;
+					<span className="price-decimal">{formattedPrice.int}.</span>
+					<span className="price-integer">{formattedPrice.dec ?? '0.0'}</span>
+					<span className="price-decimal">
+						{formattedPrice.trailing ?? '0.0'}
+					</span>
+				</span>
+			);
+		}
+	}
 }
 
 export default PriceText;
