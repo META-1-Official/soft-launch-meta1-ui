c644b182569135fc58ea43eac804f2d0237c73d7
Kou Zhang <kok@Kous-Mac-mini.local>
2022-05-20 04:38:31 +0800

    new fix

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import AppInit from './AppInit';
 import {Global, ThemeProvider} from '@emotion/react';
 import themeDark from './lib/styles/themeDark';
 import GlobalStyles from 'lib/styles/GlobalStyles';
+import Tap from '@tapfiliate/tapfiliate-js';
 
 if (__PERFORMANCE_DEVTOOL__) {
 	const {registerObserver} = require('react-perf-devtool');
@@ -12,6 +13,8 @@ if (__PERFORMANCE_DEVTOOL__) {
 
 const rootEl = document.getElementById('content');
 const render = () => {
+	Tap.init('30344-f2b126');
+
 	ReactDOM.render(
 		<ThemeProvider theme={themeDark}>
 			<Global styles={GlobalStyles(themeDark)} />
