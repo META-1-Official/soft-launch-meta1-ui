81dc1b8713092dd263e9f7495d423f42ee88f50b
smartworld-dm <smartworld@email.com>
2022-06-13 10:33:07 +0200

    fix: Only Meta1 info when login issue

--- app/actions/MarketsActions.js
+++ app/actions/MarketsActions.js
@@ -47,8 +47,6 @@ class MarketsActions {
 	}
 
 	getMarketStats(base, quote, refresh = false, errorCallback = null) {
-		console.log('@160 - ');
-		console.trace();
 		const {marketName, first, second} = marketUtils.getMarketName(base, quote);
 		return (dispatch) => {
 			if (base === quote) return;
