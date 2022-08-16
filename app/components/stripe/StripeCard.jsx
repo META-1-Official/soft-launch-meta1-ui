c9f43e4859446df10250aafd2d36a7c9f8583571
kumarsunnykbs <kumarsunny9@gmail.com>
2022-08-16 18:20:46 +0530

    add stripe public key in env

--- app/components/stripe/StripeCard.jsx
+++ app/components/stripe/StripeCard.jsx
@@ -1,11 +1,9 @@
 import {loadStripe} from '@stripe/stripe-js';
 import {Elements} from '@stripe/react-stripe-js';
-
 import CheckoutForm from './CheckoutForm';
-const PUBLIC_KEY =
-	'pk_test_51LQOlaSJd95hxgt3TeryBYxRjTBpGCvPEyu9r9ewAAlqU2bJduADqy0Y63UJPt0UFsSY5aqFRwBbQYhLsMxCZN6Z00sOKxTsmL';
+
 const StripeCard = () => {
-	const stripeTestPromise = loadStripe(PUBLIC_KEY);
+	const stripeTestPromise = loadStripe(process.env.STRIPE_PUBLIC_KEY);
 	return (
 		<div>
 			<Elements stripe={stripeTestPromise}>
