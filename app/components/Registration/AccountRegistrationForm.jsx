44c8e3c051963ff5f1d0f9f9fe1ff882bfd7978d
Royalone94 <greatroyalone@outlook.com>
2022-09-14 10:16:01 -0400

    fix  not getting validaation message in email field on entering digits in domain name and extension field

--- app/components/Registration/AccountRegistrationForm.jsx
+++ app/components/Registration/AccountRegistrationForm.jsx
@@ -279,7 +279,7 @@ class AccountRegistrationForm extends React.Component {
 										required: true,
 										validator: (_, value) => {
 											if (
-												/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
+												/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
 													value
 												)
 											) {
