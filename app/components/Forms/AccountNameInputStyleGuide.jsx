9322c73b115979db5bea2a388226bf7169f3c9bc
Royalone94 <greatroyalone@outlook.com>
2022-10-21 10:18:07 -0400

    fix create new wallet with wallet name without having dash and numbers in registration page

--- app/components/Forms/AccountNameInputStyleGuide.jsx
+++ app/components/Forms/AccountNameInputStyleGuide.jsx
@@ -96,6 +96,21 @@ class AccountNameInput extends React.Component {
 		return error;
 	}
 
+	hasNumber(myString) {
+		return /\d/.test(myString);
+	}
+
+	isVowelsExistAndHasNumber(str) {
+		if (this.hasNumber(str)) {
+			if (str.includes('-')) {
+				return true;
+			}
+			return false;
+		} else {
+			return false;
+		}
+	}
+
 	validateAccountName(value) {
 		this.state.error =
 			value === ''
@@ -118,6 +133,11 @@ class AccountNameInput extends React.Component {
 				this.state.warning = counterpart.translate(
 					'account.name_input.premium_name_warning'
 				);
+		}
+		if (!this.state.error && !this.isVowelsExistAndHasNumber(value)) {
+			this.state.error = counterpart.translate(
+				'account.name_input.name_with_dash_number'
+			);
 		}
 		this.setState({
 			value: value,
