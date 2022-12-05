4e96907c17ce4fcf66f916b95736d05d20becc03
Royalone94 <stuntblitz@gmail.com>
2022-12-05 13:05:17 -0800

    auto login is not working after account registration

--- app/components/Registration/AccountRegistrationConfirm.jsx
+++ app/components/Registration/AccountRegistrationConfirm.jsx
@@ -254,8 +254,24 @@ class AccountRegistrationConfirm extends React.Component {
 			WalletUnlockActions.change();
 			if (resolve) resolve();
 			WalletUnlockActions.cancel();
-			ss.remove('account_login_name');
-			this.props.history.push(`/account/${account}`);
+			axios
+				.post(process.env.LITE_WALLET_URL + '/login', {
+					accountName: account,
+					email: email,
+				})
+				.then((response) => {
+					console.log('LW login response', response); // DEBUG
+					ss.set('account_login_name', response.data['accountName']);
+					ss.set('account_login_token', response.data['token']);
+					WalletUnlockActions.unlock_v2().finally(() => {
+						this.props.history.push(`/account/${account}`);
+					});
+					setTimeout(() => {
+						WalletUnlockActions.lock_v2().finally(() => {
+							this.props.history.push('/market/META1_USDT');
+						});
+					}, 24 * 60 * 60 * 1000); // Auto timeout in 24 hrs
+				});
 		}
 		this.props.history.push(
 			`/account/${account}/permissions/?currentDisplay=createPaperWallet`
