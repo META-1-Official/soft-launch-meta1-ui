6739934a03e35e16e229f42c663e9b373af71067
kumarsunnykbs <kumarsunny9@gmail.com>
2022-12-26 12:49:40 +0530

    center download paper wallet modal dev2

--- app/components/Registration/AccountRegistrationConfirm.jsx
+++ app/components/Registration/AccountRegistrationConfirm.jsx
@@ -1152,6 +1152,7 @@ class AccountRegistrationConfirm extends React.Component {
 				</Form>
 				{/* download paper wallet modal */}
 				<Modal
+					centered
 					visible={this.state.downloadPaperWalletModal}
 					className="unlock_wallet_modal2 hide-close-btn"
 					id={'downloadPaperWallet'}
