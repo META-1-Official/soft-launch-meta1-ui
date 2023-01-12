cff81ae63621be469c2fb538f7f7ec4dc57c12d9
Royalone94 <stuntblitz@gmail.com>
2023-01-12 07:39:51 -0800

    implement immediate theme change from settings dropdown

--- app/index.tsx
+++ app/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import AppInit from './AppInit';
 import {Global, ThemeProvider} from '@emotion/react';
 import themeDark from './lib/styles/themeDark';
+import themeLight from './lib/styles/themeLight';
 import GlobalStyles from 'lib/styles/GlobalStyles';
 import Tap from '@tapfiliate/tapfiliate-js';
-
 import {ToastContainer} from 'react-toastify';
+import SettingsStore from 'stores/SettingsStore';
+import {useState, useEffect} from 'react';
 
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -16,16 +18,31 @@ if (__PERFORMANCE_DEVTOOL__) {
 }
 
 const rootEl = document.getElementById('content');
+
+function Root() {
+	const [theme, setTheme] = useState(
+		SettingsStore.getState().settings.get('themes')
+	);
+
+	useEffect(() => {
+		SettingsStore.listen(function (state) {
+			setTheme(SettingsStore.getState().settings.get('themes'));
+		});
+	}, []);
+
+	return (
+		<ThemeProvider theme={theme === 'darkTheme' ? themeDark : themeLight}>
+			<Global
+				styles={GlobalStyles(theme === 'darkTheme' ? themeDark : themeLight)}
+			/>
+			<AppInit />
+		</ThemeProvider>
+	);
+}
+
 const render = () => {
 	Tap.init('30344-f2b126');
-
-	ReactDOM.render(
-		<ThemeProvider theme={themeDark}>
-			<Global styles={GlobalStyles(themeDark)} />
-			<AppInit />
-			<ToastContainer theme="dark" />
-		</ThemeProvider>,
-		rootEl
-	);
+	ReactDOM.render(<Root />, rootEl);
 };
+
 render();
