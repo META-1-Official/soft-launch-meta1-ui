2d768b1119e84d4a72d3dc200f426f893083807d
Royalone94 <stuntblitz@gmail.com>
2023-02-20 08:16:25 -0800

    configure sentry initialization

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,8 @@ import {Global, ThemeProvider} from '@emotion/react';
 import themeDark from './lib/styles/themeDark';
 import GlobalStyles from 'lib/styles/GlobalStyles';
 import Tap from '@tapfiliate/tapfiliate-js';
+import * as Sentry from '@sentry/react';
+import {BrowserTracing} from '@sentry/tracing';
 
 import {ToastContainer} from 'react-toastify';
 
@@ -14,6 +16,16 @@ if (__PERFORMANCE_DEVTOOL__) {
 	const {registerObserver} = require('react-perf-devtool');
 	registerObserver();
 }
+
+Sentry.init({
+	dsn: 'https://b1a1b9eec8bf452b8750a9b2a21b342b@o4504452886560768.ingest.sentry.io/4504713148039169',
+	integrations: [new BrowserTracing()],
+
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 1.0,
+});
 
 const rootEl = document.getElementById('content');
 const render = () => {
