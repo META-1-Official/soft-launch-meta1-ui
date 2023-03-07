9d10690c311d182529e1f136d2440f4d40b2e32b
kumarsunnykbs <kumarsunny9@gmail.com>
2023-03-07 18:38:07 +0530

    theme ui working dev2

--- app/components/Utility/SectionHeader.jsx
+++ app/components/Utility/SectionHeader.jsx
@@ -3,19 +3,22 @@ import {FormattedRelative} from 'react-intl';
 import {ChainStore} from 'meta1-vision-js';
 import PropTypes from 'prop-types';
 import {Tooltip} from 'antd';
+import {useTheme} from '@emotion/react';
 
 const SectionHeader = ({title}) => {
+	const theme = useTheme();
+
 	return (
 		<div
 			style={{
-				background: '#1C1F27',
+				background: `${theme.colors.background}`,
 				fontSize: '16px',
 				height: '46px',
 				width: '100%',
 				display: 'flex',
 				alignItems: 'center',
 				paddingLeft: '10px',
-				color: 'white',
+				color: `${theme.colors.buttonWhiteColor}`,
 			}}
 		>
 			{title}
