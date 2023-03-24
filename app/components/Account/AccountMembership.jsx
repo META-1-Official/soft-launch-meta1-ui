c552a733a1a4d8c7254b4aed421ae1d9b8c6d2f6
antmanstar <antman357357@gmail.com>
2023-03-24 21:40:06 +0800

    onramper migration

--- app/components/Account/AccountMembership.jsx
+++ app/components/Account/AccountMembership.jsx
@@ -171,11 +171,11 @@ class AccountMembership extends React.Component {
 													<a
 														href={
 															getWalletURL() +
-															`/registration/?ref=${account.name}`
+															`/registration/?r=${account.name}`
 														}
 													>
 														{getWalletURL() +
-															`/registration/?ref=${account.name}`}{' '}
+															`/registration/?r=${account.name}`}{' '}
 													</a>
 												</p>
 											</div>
