19de778289bd13fa2b8b24a933c786b0e975c597
smartworld-dm <25503283+smartworld-dm@users.noreply.github.com>
2023-06-05 02:31:47 -0700

    mod: tiny amount issue

--- app/components/Exchange/MarketOrderTab.jsx
+++ app/components/Exchange/MarketOrderTab.jsx
@@ -222,8 +222,8 @@ const MarketOrderForm = (props) => {
 				estPrice *
 				Math.pow(10, buyAsset.get('precision') - sellAsset.get('precision'));
 
-			if (estPrice < price) {
-				while (estPrice < price && delta < 100) {
+			if (floorFloat(estPrice, 5) <= price) {
+				while (floorFloat(estPrice, 5) <= price && delta < 5000) {
 					delta += 1;
 					_sellAmount += 1;
 					estPrice = _sellAmount / estBuyAmount;
@@ -241,8 +241,8 @@ const MarketOrderForm = (props) => {
 				estPrice *
 				Math.pow(10, sellAsset.get('precision') - buyAsset.get('precision'));
 
-			if (estPrice > price) {
-				while (estPrice > price && delta < 100) {
+			if (floorFloat(estPrice, 5) >= price) {
+				while (floorFloat(estPrice, 5) >= price && delta < 5000) {
 					delta += 1;
 					_sellAmount -= 1;
 					estPrice = estBuyAmount / _sellAmount;
