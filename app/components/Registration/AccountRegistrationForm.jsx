f6fe8b1d985594ea193da60a5f5b61ad7f530c51
kumarsunnykbs <kumarsunny9@gmail.com>
2023-10-02 16:43:58 +0530

    Exchange ui dashboard ui biometric-auth fix

--- app/components/Registration/AccountRegistrationForm.jsx
+++ app/components/Registration/AccountRegistrationForm.jsx
@@ -43,7 +43,7 @@ class AccountRegistrationForm extends React.Component {
 			phone: '',
 			firstname: '',
 			lastname: '',
-			captcha: false,
+			captcha: true,
 			country: 227,
 			selectedCountryObj: {
 				id: 227,
